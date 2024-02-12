1c8f6b3ad8c50952c9e1da77c19a1f70ee517713
MMartindale3 <mmartindale3@nait.ca>
2024-02-11 21:18:53 -0700

    First upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function strong(string) {
+    return `<strong>${string}</strong>`
+}
+
+const template = strong('making things useful')
+console.log(template)
+
+
+const updateInnerHTML = function(selector, htmlString){
+    document.querySelector(selector).insertAdjacentHTML('beforeend', htmlString)
+}
+
+updateInnerHTML('section', template)
